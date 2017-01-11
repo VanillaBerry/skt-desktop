27443e913efb64825588dbf716a3a5c2f84d91f9
VanillaBerry <vanilla.berry.cake@gmail.com>
2017-01-12 00:58:04 +0300

    обработка изображения
    проблема: съедаются чернила. скорее всего из-за того, что qGray работает
    так:
    The gray value is calculated using the formula (R * 11 + G * 16 + B *
    5)/32; the alpha-channel is ignored.
    
    Т.е. "вклад" синего цвета непропорционален вкладу остальных цветов.
    
    Завтра: написать другую функцию для возврата серого. Вынести обработку
    изображения в отдельный файл, м.б. класс. Отпинать соратника, чтобы
    шевелился.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,119 @@ bool toGray(QImage &img_orig){
 return true;
 };
 
+void Blur(QImage &img){
+    int h_img = img.height();
+    int w_img = img.width();
+
+    int** img_matrix=new int* [w_img];
+    float** img_matrix_av1=new float*[w_img];
+    float** img_matrix_av2=new float*[w_img];
+    float** img_matrix_new=new float*[w_img];
+
+
+    for(int i=0; i<w_img; ++i){
+        img_matrix[i]=new int[h_img];
+        img_matrix_av1[i]=new float[h_img];
+        img_matrix_av2[i]=new float[h_img];
+        img_matrix_new[i]=new float[h_img];
+    };
+
+
+    QRgb one_point;
+
+    for(int i=0; i<h_img; ++i)
+    for(int j=0; j<w_img; ++j)
+    {
+        one_point=img.pixel(j,i);
+        img_matrix[j][i]=qGray(one_point);
+    };
+
+    float gauss[5];
+    gauss[0]=0.028087;
+    gauss[1]=0.23431;
+    gauss[2]=0.475207;
+    gauss[3]=gauss[1];
+    gauss[4]=gauss[0];
+
+
+    for(int i=2; i<h_img-2; ++i)
+    for(int j=0; j<w_img; ++j)
+    {
+        img_matrix_av1[j][i]=gauss[0]*img_matrix[j][i-2];
+        img_matrix_av1[j][i]+=gauss[1]*img_matrix[j][i-1];
+        img_matrix_av1[j][i]+=gauss[2]*img_matrix[j][i];
+        img_matrix_av1[j][i]+=gauss[3]*img_matrix[j][i+1];
+        img_matrix_av1[j][i]+=gauss[4]*img_matrix[j][i+2];
+
+    };
+
+    for(int j=0; j<w_img; ++j)
+    {
+        img_matrix_av1[j][0]=img_matrix[j][0];
+        img_matrix_av1[j][1]=img_matrix[j][1];
+        img_matrix_av1[j][h_img-2]=img_matrix[j][h_img-2];
+        img_matrix_av1[j][h_img-1]=img_matrix[j][h_img-1];
+    };
+
+
+
+    for(int i=0; i<h_img; ++i)
+    for(int j=2; j<w_img-2; ++j)
+    {
+        img_matrix_av2[j][i]=gauss[0]*img_matrix_av1[j-2][i];
+        img_matrix_av2[j][i]+=gauss[1]*img_matrix_av1[j-1][i];
+        img_matrix_av2[j][i]+=gauss[2]*img_matrix_av1[j][i];
+        img_matrix_av2[j][i]+=gauss[3]*img_matrix_av1[j+1][i];
+        img_matrix_av2[j][i]+=gauss[4]*img_matrix_av1[j+2][i];
+
+    };
+
+    for(int i=0; i<h_img; ++i)
+    {
+        img_matrix_av2[0][i]=img_matrix_av1[0][i];
+        img_matrix_av2[1][i]=img_matrix_av1[1][i];
+        img_matrix_av2[w_img-2][i]=img_matrix_av1[w_img-2][i];
+        img_matrix_av2[w_img-1][i]=img_matrix_av1[w_img-1][i];
+
+    };
+
+
+
+    int new_value;
+
+    for(int i=2; i<h_img-2; ++i)
+    for(int j=2; j<w_img-2; ++j)
+    {
+       new_value=floor(455*img_matrix[j][i]/(img_matrix_av2[j][i]+1))-200;
+
+        if (new_value>255) new_value=255;
+        if (new_value<0) new_value=0;
+
+        one_point=qRgb(new_value,new_value,new_value);
+        img.setPixelColor(j,i,one_point);
+
+    };
+
+
+
+
+
+    for(int i=0; i<w_img; ++i){
+        delete [] img_matrix[i];
+        delete [] img_matrix_av1[i];
+        delete [] img_matrix_av2[i];
+        delete [] img_matrix_new[i];
+    };
+
+
+    delete [] img_matrix;
+    delete [] img_matrix_av1;
+    delete [] img_matrix_av2;
+    delete [] img_matrix_new;
+
+};
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -39,23 +152,11 @@ int main(int argc, char *argv[])
      QImage img_orig, img_rgb32, img_gray, img_trns;
      img_orig.load("../skt-desktop/bitmaps/geometriche.bmp");
 
-     int h_img = img_orig.height();
-     int w_img = img_orig.width();
-
-     toGray(img_orig);
      img_rgb32=img_orig.convertToFormat(QImage::Format_RGB32, Qt::AutoColor);
+     toGray(img_orig);
 
-//koefficienty dlya razmitia po gaussu
-    float gauss[5];
-    gauss[0]=0.028087;
-    gauss[1]=0.23431;
-    gauss[2]=0.475207;
-    gauss[3]=gauss[1];
-    gauss[4]=gauss[0];
-//koefficienty dlya razmitia po gaussu
-
-
-
+     Blur(img_orig);
+//    Contrast(img_orig);
 
 
 //   QImage n_img = img->scaled(1200, 900, Qt::IgnoreAspectRatio, Qt::FastTransformation);
@@ -68,6 +169,7 @@ int main(int argc, char *argv[])
     lbl.setPixmap(QPixmap::fromImage(img_orig, Qt::AutoColor));
 
     lbl.show();
+    img_orig.save("E:/GitHub/Repositories/skt-desktop/bitmaps/geometriche3.bmp",0,100);
 
     return a.exec();
 }
